64d2c876fc0e9f5bfcf264f84076fd15ce0cd669
Jason Milam <jasonlmilam@gmail.com>
2018-05-22 12:09:12 -0400

    Reformat entry screen for better user experience

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,9 +3,9 @@ class Category < ApplicationRecord
 
 	validates :name, presence: true
 
-	enum style_tags: { 1 => { "background-color" => "bg-light", "border" => "border-light", "text-color" => "text-light" },
-										 2 => { "background-color" => "bg-info", "border" => "border-info", "text-color" => "text-info"},
-										 3 => { "background-color" => "bg-success", "border" => "border-success", "text-color" => "text-success"},
-   									 4 => { "background-color" => "bg-warning", "border" => "border-warning", "text-color" => "text-warning"},
-   									 5 => { "background-color" => "bg-primary", "border" => "border-primary", "text-color" => "text-primary"} }
+	enum style_tags: { 1 => { "background-color" => "bg-light", "border" => "border-light", "text-color" => "text-light", "thead-color" => "bg-light" },
+										 2 => { "background-color" => "bg-info", "border" => "border-info", "text-color" => "text-info", "thead-color" => "bg-info"},
+										 3 => { "background-color" => "bg-success", "border" => "border-success", "text-color" => "text-success", "thead-color" => "bg-success"},
+   									 4 => { "background-color" => "bg-warning", "border" => "border-warning", "text-color" => "text-warning", "thead-color" => "bg-warning"},
+   									 5 => { "background-color" => "bg-primary", "border" => "border-primary", "text-color" => "text-primary", "thead-color" => "bg-primary"} }
 end
--- spec/models/workout_group_spec.rb
+++ spec/models/workout_group_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe WokoutGroup, type: :model do
+RSpec.describe WorkoutGroup, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end

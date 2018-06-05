2a3e11b7cb81bd3a0f34da1d68879acf3dbacdb7
Jason Milam <jasonlmilam@gmail.com>
2018-06-05 11:20:39 -0400

    Fill nil workout with 0

--- app/models/user_previous_workout.rb
+++ app/models/user_previous_workout.rb
@@ -10,20 +10,41 @@ class UserPreviousWorkout < ApplicationRecord
 
 	def self.for_google_charts(grouped_workouts)
 		workout_stats = {}
+    
 		grouped_workouts.each do |previous_workout|
       workout_details = ['Date']
       previous_workout[1].each do |prev_workout|
-        exercise_names = prev_workout.exercises.map(&:name)
+        exercises = prev_workout.exercises.sort
+        exercise_names = exercises.map(&:name)
+        exercise_ids = exercises.map(&:id)
         if workout_stats["#{prev_workout.workout_group.name}"].nil? || workout_stats["#{prev_workout.workout_group.name}"].include?(exercise_names[0])
           workout_details << exercise_names 
           workout_stats["#{prev_workout.workout_group.name}"] = [workout_details.flatten]
         end
         workout_stats["#{prev_workout.workout_group.name}"] <<
-            [prev_workout.workout_date.strftime('%m/%d/%y'),
-             prev_workout.workout_details.map { |detail| detail.max_rep_weight }].flatten
+            [prev_workout.workout_date.strftime('%m/%d/%y'), self.build_max_reps(prev_workout.workout_details, exercise_ids)].flatten
       end
     end
 
     workout_stats
 	end
+
+  def self.build_max_reps(details, exercise_ids)
+    max_reps = Array.new(exercise_ids.count)
+    
+    detail_exercise_ids = details.map(&:exercise_id).sort
+
+    exercise_ids.each_with_index do |id, idx|
+      if !detail_exercise_ids.include?(id)
+        max_reps[idx] = 0
+      end
+    end
+
+    details.each do |detail|
+      idx = exercise_ids.index(detail.exercise_id)
+      max_reps[idx] = detail.max_rep_weight
+    end
+    max_reps
+  end
+
 end

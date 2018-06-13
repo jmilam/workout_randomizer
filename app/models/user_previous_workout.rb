8629bb36ea67a052788a4f6855f5d81191444c89
Jason Milam <jasonlmilam@gmail.com>
2018-06-13 09:09:23 -0400

    Regroup charts

--- app/models/user_previous_workout.rb
+++ app/models/user_previous_workout.rb
@@ -12,21 +12,24 @@ class UserPreviousWorkout < ApplicationRecord
 		workout_stats = {}
     
 		grouped_workouts.each do |previous_workout|
-      p workout_name = WorkoutGroup.find(previous_workout[0]).workout.name
+      workout_name = WorkoutGroup.find(previous_workout[0]).workout.name
       workout_details = ['Date']
       previous_workout[1].each do |prev_workout|
         exercises = prev_workout.exercises.sort
         exercise_names = exercises.map(&:name)
         exercise_ids = exercises.map(&:id)
 
+        workout_details << exercise_names
+
         if workout_stats["#{workout_name}"].nil?
           workout_stats["#{workout_name}"] = {}
-        elsif workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"].nil? || workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"].include?(exercise_names[0])
-          workout_details << exercise_names 
           workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"] = [workout_details.flatten]
-          workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"] <<
-            [prev_workout.workout_date.strftime('%m/%d/%y'), self.build_max_reps(prev_workout.workout_details, exercise_ids)].flatten
+        elsif workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"].nil? || workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"].include?(exercise_names[0])
+          workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"] = [workout_details.flatten]
         end
+
+        workout_stats["#{workout_name}"]["#{prev_workout.workout_group.name}"] <<
+          [prev_workout.workout_date.strftime('%m/%d/%y'), self.build_max_reps(prev_workout.workout_details, exercise_ids)].flatten
       end
     end
 

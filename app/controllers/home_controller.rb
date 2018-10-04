8c06967431eb22c4d6140ff1b3beb5f5b226f834
Jason Milam <jasonlmilam@gmail.com>
2018-10-04 09:09:12 -0400

    Clean up code smell

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
 	layout 'home'
 	skip_before_action :authenticate_user!
 	def index
-		p current_user
 	end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # GET /resource/sign_in
   def new
-    @image = '/assets/images/adult-back-view-black-and-white-759817'
     super
   end
 

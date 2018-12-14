68c40585451730e5f774727aa727b4377f20302b
Jason Milam <jasonlmilam@gmail.com>
2018-12-14 08:47:21 -0500

    Add bcc

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
 	def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: 'Welcome to Boomslang Fitness!')
+    mail(to: @user.email, bcc: ENV['EMAIL'], subject: 'Welcome to Boomslang Fitness!')
   end
 end

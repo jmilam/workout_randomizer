a6e744a1e045a38efe8dbac3221beaf87af9edf1
Jason Milam <jasonlmilam@gmail.com>
2019-01-17 14:20:06 -0500

    Debug logic

--- app/controllers/kiosk_controller.rb
+++ app/controllers/kiosk_controller.rb
@@ -42,11 +42,12 @@ class KioskController < ApplicationController
         previous_workout_ids = workout_group_ids.map { |group_id| WorkoutGroup.find(group_id).workout&.id }.uniq
         # Delete workout from results if user has already done the workout
         sorted_workouts.delete_if { |workout| previous_workout_ids.include?(workout&.id)}
+        p sorted_workouts
         # Select best workout for user
         @workout = sorted_workouts.empty? ? @user.gym.workouts.sample : sorted_workouts[0]
         # Assign workout to user
         @user.update(current_workout: @workout&.id,
-                     current_workout_group: @workout.workout_groups.sample.id)
+                     current_workout_group: @workout&.workout_groups.sample.id)
       else
         @workout = Workout.find(@user.current_workout)
         workouts_complete = WorkoutDetail

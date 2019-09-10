f3904e64173a5c3580aebd1ed8318edd59acb10b
Jason Milam <jasonlmilam@gmail.com>
2019-09-10 15:09:55 -0400

    Add gym management user screen

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,13 +12,36 @@ class UserController < ApplicationController
 	end
 
   def create
-    p @user = User.new(user_params)
+    @user = User.new(user_params)
+    @user.password = params[:user][:pin]
+    @user.gym = current_user.gym
+
+    #store values in case error
+    @measurements = @user.measurements.build
+    @regularity = User.regularities
+    @goals = User.goals
+    @gyms = Gym.all.includes(:users)
+    @trainers = current_user.gym.users.trainers
+    @workouts = Workout.all
+    @current_gym = current_user.gym
+
+    if @user.save
+      redirect_to gym_path(current_user.gym_id)
+    else
+      error_string = ""
+      @user.errors.messages.each do |key, messages|
+        error_string << "#{key}: " + messages.join(',') + "\n"
+      end
+      flash[:alert] = error_string
+      render new_user_path
+    end
   end
 
   protected
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :height, :weight, :regularity_id, :goal_id, :trainer_id,
+    params.require(:user).permit(:first_name, :last_name, :height, :weight, :pin, :regularity_id, :goal_id, :trainer_id,
+                                 :email, :phone_number,
       measurements: [:upper_arm, :chest, :waist, :hip, :thigh, :calf, :wrist, :forearm, :left_tricep, :right_tricep,
                      :subscapular, :abdominal, :mid_thigh, :inside_calf, :pec, :left_bicep, :right_bicep, :suprailiac])
   end

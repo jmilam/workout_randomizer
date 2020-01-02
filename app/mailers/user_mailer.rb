f60c1068b7a1aeff7e3a34a66dfdde5762f62845
Jason Milam <jasonlmilam@gmail.com>
2020-01-02 13:46:49 -0500

    Add nutrition plan attachment

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
 	def welcome_email
     @user = params[:user]
+    attachments['Nutrition Plan.docx'] = File.read('lib/Nutrition Plan.docx')
     mail(to: @user.email, bcc: ENV['EMAIL'], subject: 'Welcome to Boomslang Fitness!')
   end
 
   def more_info_email
-    p "Mailer"
     @user = params[:user]
 
     mail(to: ENV['EMAIL'], subject: 'Requesting More Info')

27179ab7363a76ff61c52b10c0920c101cee9fa6
Jason Milam <jasonlmilam@gmail.com>
2020-04-07 07:51:43 -0400

    Resolve registration issue

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-
+  layout 'sign_up'
   # GET /resource/sign_up
   def new
     @user = User.new(email: params.dig(:user, :email), not_a_robot: false)

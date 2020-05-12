66d14677755dd43f2e2822a19ac7295ec3ec862f
Jason Milam <jasonlmilam@gmail.com>
2020-05-12 09:42:18 -0400

    Add application level gym set

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_gym
   protect_from_forgery with: :null_session
+
+  def set_gym
+    @gym = current_user&.gym
+  end
 end

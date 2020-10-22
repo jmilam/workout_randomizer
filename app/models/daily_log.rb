29f461d47727ff110094425a2afc2f552c2f58ed
Jason Milam <jasonlmilam@gmail.com>
2020-10-22 07:53:19 -0400

    Update total calorie calculator

--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -4,18 +4,22 @@ class DailyLog < ApplicationRecord
   has_many :foods, through: :daily_log_foods
 
   def total_calories
-    daily_log_foods.map { |food_details| food_details.food.calories * food_details.qty }.sum
+    daily_log_foods.map { |food_details| calculate_by_macro(food_details.food&.calories, food_details.qty) }.sum
   end
 
   def total_protein
-    daily_log_foods.map { |food_detials| food_detials.food.protein * food_detials.qty }.sum
+    daily_log_foods.map { |food_detials| calculate_by_macro(food_detials.food&.protein, food_detials.qty) }.sum
   end
 
   def total_carbs
-    daily_log_foods.map { |food_detials| food_detials.food.carbs * food_detials.qty }.sum
+    daily_log_foods.map { |food_detials| calculate_by_macro(food_detials.food&.carbs, food_detials.qty) }.sum
   end
 
   def total_fats
-    daily_log_foods.map { |food_detials| food_detials.food.fat * food_detials.qty }.sum
+    daily_log_foods.map { |food_detials| calculate_by_macro(food_detials.food&.fat, food_detials.qty) }.sum
+  end
+
+  def calculate_by_macro(food_macro_count, qty)
+    (food_macro_count || 0) * qty
   end
 end
